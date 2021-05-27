7122c205ca91a710f894e56150a2bf2949f5027d
bowaer <cb229435444@outlook.com>
2021-05-27 14:18:15 +0800

    fix: setup 引用路径必须是相对路径

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup
 from os import path
 
 BASE_DIR = path.dirname(path.abspath(__file__))
-README_DIR = path.join(BASE_DIR, 'README.md')
+README_DIR = 'README.md'
 with open(path.join(BASE_DIR, 'requirements.txt'), encoding='utf-8') as f:
     requirements = f.read().split()
 
 setup(
     name='blotly',
-    version='1.0.3',
+    version='1.0.4',
     description='基于cufflinks的绘图工具',
     author='bowaer',
     author_email='cb229435444@outlook.com',
